5a7e5b7691e84dbcc1ee52934ab54d6c8e8805f0
Jangwon <wone45@hanmail.net>
2023-12-07 23:41:07 +0900

    add axios

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,14 +1,38 @@
 import { useParams, Link } from 'react-router-dom'
-
+import axios from 'axios'
 export default function WelcomeComponent() {
 
     const { username } = useParams()
+
+    function callHelloRestApi() {
+        console.log('get called')
+        
+        axios.get('http://localhost:8080/test')
+            .then(
+                (res) => successfulResponse(res))
+            .catch(
+                (error) => errorResponse(error))
+            .finally(() => console.log('clean up'))
+    }
+
+    function successfulResponse(res) {
+        console.log(res)
+    }
+
+    function errorResponse(error) {
+        console.log(error)
+    }
 
     return (
         <div className="WelcomeComponent">
             <h1>Welecome {username}</h1>
             <div className="Welcome">
                 Manage Your todos - <Link to="/todos">Go here</Link>
+            </div>
+            <div>
+                <button className="btn btn-success" onClick={callHelloRestApi}>
+                    Call hello world
+                </button>
             </div>
         </div>
     )
