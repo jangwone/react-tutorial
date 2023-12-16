5cd58221777b1155209754c188634a0fe64713f7
jangwone <wone45@hanmail.net>
2023-12-16 15:32:34 +0900

    로그인 시 기본인증

--- src/components/todo/api/HellowWorldApiService.js
+++ src/components/todo/api/HellowWorldApiService.js
@@ -4,5 +4,17 @@ const apiClient = axios.create({
   baseURL: "http://localhost:8080",
 });
 
+//Reponse to preflight request doesn't pass access control check  (error)
 export const returnHelloWorldBean = (username) =>
-  apiClient.get(`/test/path/${username}`);
+  apiClient.get(`/test/path/${username}`,{
+    headers: { 
+      Authorization:'Basic d29uZTpkdW1teQ=='
+    }
+  });
+
+export const executeBasicAuthenticationService 
+= (token) =>apiClient.get(`/basicauth`,{
+    headers: { 
+      Authorization:token
+    }
+  });
--- src/components/todo/security/AuthContext.js
+++ src/components/todo/security/AuthContext.js
@@ -1,39 +1,58 @@
-import { useContext } from 'react'
-import { createContext, useState } from 'react';
+import { useContext } from "react";
+import { createContext, useState } from "react";
+import { executeBasicAuthenticationService } from "../api/HellowWorldApiService";
 //1. Create a Context
-export const AuthContext = createContext()
+export const AuthContext = createContext();
 
 export const useAuth = () => useContext(AuthContext);
 
 //2. Share the create context with other components
 export default function AuthProvider({ children }) {
+  //3: put some in the context
+  const [isAuthenticated, setAuthenticated] = useState(false);
 
-    //3: put some in the context 
-    const [isAuthenticated, setAuthenticated] = useState(false)
-
-    const [username, setUsername] = useState(null)
-
-    function login(username, password) {
-        if (username === 'wone' && password === 'dummy') {
-            setAuthenticated(true)
-            setUsername(username)
-            return true
-        }
-        else {
-            setAuthenticated(false)
-            setUsername(null)
-            return false
-        } 
+  const [username, setUsername] = useState(null);
+/*
+  function login(username, password) {
+    if (username === "wone" && password === "dummy") {
+      setAuthenticated(true);
+      setUsername(username);
+      return true;
+    } else {
+      setAuthenticated(false);
+      setUsername(null);
+      return false;
     }
+  }
+*/
+  function login(username, password) {
 
-    function logout() {
-        setAuthenticated(false)
-    }
+    const baToken = 'Basic ' + window.btoa( username + ':' + password )
 
-    return (
-        <AuthContext.Provider value={ { isAuthenticated, login, logout, username} }>
-            {children}
-        </AuthContext.Provider>
-    )
+    executeBasicAuthenticationService(baToken)
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+
+    setAuthenticated(false)
+
+    // if (username === "wone" && password === "dummy") {
+    //   setAuthenticated(true);
+    //   setUsername(username);
+    //   return true;
+    // } else {
+    //   setAuthenticated(false);
+    //   setUsername(null);
+    //   return false;
+    // }
+  }
+
+  function logout() {
+    setAuthenticated(false);
+  }
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout, username }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
-
